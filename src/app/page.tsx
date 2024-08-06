ad7d9435d9955926658d03b43def1e5258773595
Aran Leite <hyoretsu@gmail.com>
2024-08-06 13:19:02 -0300

    Change ShirtTile styling and componentize it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import ColorCircle from "@components/ColorCircle";
 import prisma from "@database";
-import { Link } from "@hyoretsu/react-components";
-import Image from "next/image";
+import ShirtTile from "./components/ShirtTile";
 import styles from "./styles.module.scss";
 
 export default async function Home() {
@@ -9,29 +7,8 @@ export default async function Home() {
 
 	return (
 		<div className={styles.container}>
-			{shirtModels.map(({ colors, id, model, photos, prices }) => (
-				<div key={id} className={styles.shirtTile}>
-					<Image alt={`Camiseta ${model}`} src={photos[0]} width={230} height={344} />
-
-					<h4>{model}</h4>
-
-					<p>
-						A partir de:
-						<br />
-						<b>R${prices[0].toLocaleString()}</b>
-					</p>
-
-					<span>Cores:</span>
-					<div className={styles.shirtColors}>
-						{colors.map(color => (
-							<ColorCircle key={color} color={color} />
-						))}
-					</div>
-
-					<Link href={`/shirts/${id}`}>
-						<button type="button">Comprar</button>
-					</Link>
-				</div>
+			{shirtModels.map(shirt => (
+				<ShirtTile key={shirt.id} shirt={shirt} />
 			))}
 		</div>
 	);
